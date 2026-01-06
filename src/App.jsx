0e75f99fac7cf5ebc6e9270feb321091afd28944
Ubaid Ullah <165874963+UbaidUllah9962@users.noreply.github.com>
2026-01-06 22:21:51 +0530

    Create App.jsx component in src directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/pages/Home";
-import Images from "./components/pages/Images";
-import Navbar from "./components/pages/Navbar";
-import About from "./components/pages/About";
+import React from 'react';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/images" element={<Images />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Al-Habibi Tour - Travels</h1>
+        <p>Your gateway to amazing travel experiences</p>
+      </header>
+      <main>
+        {/* Add your main content here */}
+      </main>
+    </div>
   );
 }
 
